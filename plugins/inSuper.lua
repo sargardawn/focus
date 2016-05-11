92ab95aabdb1449ebe48835da1465cd2e683b98f
sargardan <soheil22222222@users.noreply.github.com>
2016-05-11 16:52:08 +0430

    Update inSuper.lua

--- plugins/inSuper.lua
+++ plugins/inSuper.lua
@@ -1258,7 +1258,7 @@ local function run(msg, matches)
 				resolve_username(username,  callbackres, cbres_extra)
 			else
 				savelog(msg.to.id, name_log.." ["..msg.from.id.."] requested SuperGroup ID")
-				return "ایدی گروه: "..msg.to.id.."\n اسم گروه: "\nایدی:"..msg.from.id.."\n یوزرنیم: @"..(msg.from.username or '').."			end
+				return "ایدی گروه: "..msg.to.id.."\nایدی:"..msg.from.id.."\n یوزرنیم: @"..(msg.from.username or '').."			end
 		end
 
 		if matches[1] == 'kickme' then
